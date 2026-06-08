3de21b26d2a249b273de7aa9984d7cc4bde139d6
Amit Patidar <amit.patidar6264@gmail.com>
2026-06-08 12:08:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Frontend Service Running");
+  res.send("Frontend Service Running AWS Ec2 server");
 });
 
 app.listen(3000, () => {
